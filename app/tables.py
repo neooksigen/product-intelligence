109ade3dfe4498df09ebf119e409195ac7d264f0
neooksigen <neooksigen@windowslive.com>
2026-03-24 14:20:54 +0700

    Update tables.py add MoneyExchangeRate class and its table

--- app/tables.py
+++ app/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Text, Boolean, Uuid
+from sqlalchemy import String, Integer, Text, Boolean, Uuid, Float
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database_sqlalchemy import Base 
 import uuid
@@ -20,6 +20,18 @@ class Product(Base):
     timestamp_extract: Mapped[str] = mapped_column(Text)
     questions: Mapped[str] = mapped_column(Text)
     nonparsed_response: Mapped[str] = mapped_column(Text)
+    product_name_en: Mapped[str] = mapped_column(Text) # product_name_en till product_category are new columns 21 March 2026 !
+    measurement_scale_standardized: Mapped[str] = mapped_column(Text)
+    quantity_standardized: Mapped[float] = mapped_column(Float)
+    price_local: Mapped[float] = mapped_column(Float)
+    price_usd: Mapped[float] = mapped_column(Float)
+    price_eur: Mapped[float] = mapped_column(Float)
+    price_chf: Mapped[float] = mapped_column(Float)
+    price_jpy: Mapped[float] = mapped_column(Float)
+    price_cny: Mapped[float] = mapped_column(Float)
+    price_aud: Mapped[float] = mapped_column(Float)
+    price_sgd: Mapped[float] = mapped_column(Float)
+    product_category: Mapped[str] = mapped_column(Text)
     owner_id: Mapped[uuid.UUID] = mapped_column(Uuid)
 
 class SearchQueries(Base):
@@ -51,3 +63,14 @@ class GsQueries(Base):
     active_status: Mapped[bool] = mapped_column(Boolean)
     last_run_at: Mapped[str] = mapped_column(Text)
     loop_order: Mapped[int] = mapped_column(Integer)        
+
+class MoneyExchangeRate(Base):
+    __tablename__ = "money_exchange_rate"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    from_base_code: Mapped[str] = mapped_column(Text)
+    to_currency: Mapped[str] = mapped_column(Text)
+    exchange_rate: Mapped[float] = mapped_column(Float)
+    time_last_update_utc: Mapped[str] = mapped_column(Text)
+    timestamp_extract: Mapped[str] = mapped_column(Text)
+    owner_id: Mapped[uuid.UUID] = mapped_column(Uuid)
